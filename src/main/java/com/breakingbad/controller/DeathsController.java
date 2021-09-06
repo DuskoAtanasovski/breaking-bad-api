6e535763983d78b8d24523af2dd448baa3d939ba
Atanasovski <dushko.atanosovski@streann.com>
2021-09-06 16:39:16 +0200

    added new controller layer for Deaths and Quotes

--- src/main/java/com/breakingbad/controller/DeathsController.java
+++ src/main/java/com/breakingbad/controller/DeathsController.java
@@ -1,4 +1,31 @@
 package com.breakingbad.controller;
 
+import com.breakingbad.dto.DeathsDto;
+import com.breakingbad.service.DeathsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/")
 public class DeathsController {
+
+    private final DeathsService deathsService;
+
+    @GetMapping("deaths/all")
+    public List<DeathsDto> getAll() {
+        return deathsService.getAllDeaths();
+    }
+
+    @GetMapping("deaths/save")
+    public ResponseEntity saveDeaths() {
+        deathsService.saveDeaths();
+        return new ResponseEntity("Success", HttpStatus.OK);
+    }
 }
--- src/main/java/com/breakingbad/controller/QuotesController.java
+++ src/main/java/com/breakingbad/controller/QuotesController.java
@@ -1,4 +1,31 @@
 package com.breakingbad.controller;
 
+import com.breakingbad.dto.QuotesDto;
+import com.breakingbad.service.QuotesService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/")
 public class QuotesController {
+
+    private final QuotesService quotesService;
+
+    @GetMapping("quotes/all")
+    public List<QuotesDto> getAll() {
+        return quotesService.getAllQuotes();
+    }
+
+    @GetMapping("quotes/save")
+    public ResponseEntity saveQuotes() {
+        quotesService.saveQuotes();
+        return new ResponseEntity("Success", HttpStatus.OK);
+    }
 }
