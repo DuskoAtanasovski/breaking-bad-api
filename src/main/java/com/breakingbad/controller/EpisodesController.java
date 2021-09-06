6eaf21f2d49943cf3231b2727cef5d093cafff08
Atanasovski <dushko.atanosovski@streann.com>
2021-09-06 12:00:52 +0200

    added EpisodesController layer

--- src/main/java/com/breakingbad/controller/EpisodesController.java
+++ src/main/java/com/breakingbad/controller/EpisodesController.java
@@ -1,4 +1,28 @@
 package com.breakingbad.controller;
 
+import com.breakingbad.dto.EpisodesDto;
+import com.breakingbad.service.EpisodesService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/")
 public class EpisodesController {
+
+    private final EpisodesService episodesService;
+
+    @GetMapping("episodes/all")
+    public List<EpisodesDto> getAll() {
+        return episodesService.getAllEpisodes();
+    }
+
+    @GetMapping("episodes/save")
+    public void saveEpisodes() {
+        episodesService.saveEpisodes();
+    }
 }
