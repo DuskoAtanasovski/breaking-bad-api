d9773a09b0e292e3af876ea0fa0069f048f28cf2
Atanasovski <dushko.atanosovski@streann.com>
2021-09-06 16:33:47 +0200

    added serviceImplementation for Deaths and Quotes

--- src/main/java/com/breakingbad/service/impl/DeathsServiceImpl.java
+++ src/main/java/com/breakingbad/service/impl/DeathsServiceImpl.java
@@ -1,11 +1,25 @@
 package com.breakingbad.service.impl;
 
 import com.breakingbad.dto.DeathsDto;
+import com.breakingbad.model.Deaths;
+import com.breakingbad.repository.DeathsRepository;
 import com.breakingbad.service.DeathsService;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
+@RequiredArgsConstructor
 public class DeathsServiceImpl implements DeathsService {
+
+    private final DeathsRepository deathsRepository;
+    private final ObjectMapper objectMapper;
+    private final ResourceLoader resourceLoader;
+
     @Override
     public List<DeathsDto> getAllDeaths() {
         return null;
@@ -13,6 +27,38 @@ public class DeathsServiceImpl implements DeathsService {
 
     @Override
     public void saveDeaths() {
+        try {
+            List<DeathsDto> deathsDtoList = objectMapper.readValue(resourceLoader.getResource("classpath:sources/deaths.json").getInputStream(), new TypeReference<List<DeathsDto>>() {
+            });
+            for (DeathsDto deathsDto : deathsDtoList) {
+                Deaths deaths = new Deaths();
+                deaths.setDeathId(deathsDto.getDeathId());
+                deaths.setDeath(deathsDto.getDeath());
+                deaths.setCause(deathsDto.getCause());
+                deaths.setResponsible(deathsDto.getResponsible());
+                deaths.setLastWords(deathsDto.getLastWords());
+                deaths.setSeason(deathsDto.getSeason());
+                deaths.setEpisode(deathsDto.getEpisode());
+                deaths.setNumberOfDeaths(deathsDto.getNumberOfDeaths());
+                deathsRepository.save(deaths);
+            }
+        } catch (Exception e) {
 
+        }
+    }
+
+    private DeathsDto mapFromDeathsToDto(Deaths deathsDto){
+        DeathsDto deaths = new DeathsDto();
+        deathsDto.setDeathId(deaths.getDeathId());
+        deathsDto.setDeath(deaths.getDeath());
+        deathsDto.setCause(deaths.getCause());
+        deathsDto.setResponsible(deaths.getResponsible());
+        deathsDto.setLastWords(deaths.getLastWords());
+        deathsDto.setSeason(deaths.getSeason());
+        deathsDto.setEpisode(deaths.getEpisode());
+        deathsDto.setNumberOfDeaths(deaths.getNumberOfDeaths());
+
+
+        return  deaths;
     }
 }
--- src/main/java/com/breakingbad/service/impl/QuotesServiceImpl.java
+++ src/main/java/com/breakingbad/service/impl/QuotesServiceImpl.java
@@ -1,15 +1,31 @@
 package com.breakingbad.service.impl;
 
 import com.breakingbad.dto.QuotesDto;
+import com.breakingbad.model.Quotes;
+import com.breakingbad.repository.QuotesRepository;
 import com.breakingbad.service.QuotesService;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
+@Service
+@RequiredArgsConstructor
 public class QuotesServiceImpl implements QuotesService {
+
+    private final QuotesRepository quotesRepository;
+    private final ObjectMapper objectMapper;
+    private final ResourceLoader resourceLoader;
+
     @Override
     public List<QuotesDto> getAllQuotes() {
-        return null;
+        List<Quotes> quotesList = quotesRepository.findAll();
+        return quotesList.stream().map(this::mapFromQuotesToDto).collect(Collectors.toList());
     }
 
     @Override
@@ -19,6 +35,29 @@ public class QuotesServiceImpl implements QuotesService {
 
     @Override
     public void saveQuotes() {
+        try {
+            List<QuotesDto> quotesDtoList = objectMapper.readValue(resourceLoader.getResource("classpath:sources/quotes.json").getInputStream(), new TypeReference<List<QuotesDto>>() {
+            });
+            for (QuotesDto quotesDto : quotesDtoList) {
+                Quotes quotes = new Quotes();
+                quotes.setQuoteId(quotesDto.getQuoteId());
+                quotes.setQuote(quotesDto.getQuote());
+                quotes.setAuthor(quotesDto.getAuthor());
+                quotes.setSeries(quotesDto.getSeries());
+                quotesRepository.save(quotes);
+            }
+        } catch (Exception e) {
 
+        }
+    }
+
+    private QuotesDto mapFromQuotesToDto(Quotes quotes) {
+        QuotesDto quotesDto = new QuotesDto();
+        quotesDto.setQuoteId(quotes.getQuoteId());
+        quotesDto.setQuote(quotes.getQuote());
+        quotesDto.setAuthor(quotes.getAuthor());
+        quotesDto.setSeries(quotes.getSeries());
+
+        return quotesDto;
     }
 }
